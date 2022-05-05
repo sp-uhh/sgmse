5cf60675eec91b0b4c98a14c85c928ddb85e288a
Julius Richter <jrichter@informatik.uni-hamburg.de>
2022-05-05 18:00:31 +0200

    evalutaion script normalize with noisy speech

--- evaluation.py
+++ evaluation.py
@@ -10,10 +10,12 @@ from sgmse.data_module import SpecsDataModule
 from sgmse.sdes import OUVESDE
 from sgmse.model import ScoreModel
 
+from utils import pad_spec
+
 # File directories
 #clean_dir = '/export/home/jrichter/data/WSJ0+CHiME3/test/clean'
 #noisy_dir = '/export/home/jrichter/data/WSJ0+CHiME3/test/noisy'
-sgmse_dir = '/export/home/jrichter/repos/sgmse/enhanced/wklkh13a_322/'
+sgmse_dir = '/export/home/jrichter/repos/sgmse/enhanced/haceomy_400_norm/'
 
 clean_dir = '/export/home/jrichter/data/VoiceBank/valid/clean/'
 noisy_dir = '/export/home/jrichter/data/VoiceBank/valid/noisy/'
@@ -22,8 +24,7 @@ noisy_dir = '/export/home/jrichter/data/VoiceBank/valid/noisy/'
 # Model checkpoint
 #checkpoint_file = '/export/home/jrichter/repos/score-speech/sgmse_logs/SGMSE/sweet-resonance-97/epoch=325-step=39445.ckpt'
 #checkpoint_file = '/export/home/jrichter/repos/sgmse/logs/sgmse/2haceomy/checkpoints/epoch=295-step=214303.ckpt'
-#checkpoint_file = '/export/home/jrichter/repos/sgmse/logs/sgmse/2haceomy/checkpoints/epoch=401-step=291047.ckpt'
-checkpoint_file = '/export/home/jrichter/repos/sgmse/logs/sgmse/wklkh13a/checkpoints/epoch=322-step=116925.ckpt'
+checkpoint_file = '/export/home/jrichter/repos/sgmse/logs/sgmse/2haceomy/checkpoints/epoch=401-step=291047.ckpt'
 
 # Settings
 sr = 16000
@@ -31,14 +32,6 @@ snr = 0.33
 N = 50
 corrector_steps = 1
 
-def pad_spec(Y):
-    T = Y.size(3)
-    if T%64 !=0:
-        num_pad = 64-T%64
-    else:
-        num_pad = 0
-    pad2d = torch.nn.ZeroPad2d((0, num_pad, 0,0))
-    return pad2d(Y)
 
 # Load score model 
 model = ScoreModel.load_from_checkpoint(
@@ -55,14 +48,12 @@ for noisy_file in tqdm(noisy_files):
     clean_file = '{}/{}'.format(clean_dir, filename)
     diffuse_file = '{}/{}'.format(sgmse_dir, filename)
     
-    # Load wavs
-    x, _ = load(clean_file)
+    # Load wav
     y, _ = load(noisy_file) 
-    T_orig = x.size(1)   
+    T_orig = y.size(1)   
 
-    # Normalize per utterance
-    norm_factor = x.abs().max()
-    x = x / norm_factor
+    # Normalize
+    norm_factor = y.abs().max()
     y = y / norm_factor
     
     # Prepare DNN input
@@ -73,13 +64,15 @@ for noisy_file in tqdm(noisy_files):
     sampler = model.get_pc_sampler(
         'reverse_diffusion', 'ald', Y.cuda(), N=N, 
         corrector_steps=corrector_steps, snr=snr)
-    sample, reverse_samples, xt1, full_n_steps = sampler()
+    sample = sampler()
     
     # Backward transform in time domain
     x_hat = model.to_audio(sample.squeeze(), T_orig)
-    x_hat = x_hat.cpu()
+
+    # Renormalize
+    x_hat = x_hat * norm_factor
 
     # Write enhanced wav file
-    write(sgmse_dir+filename, x_hat.numpy(), 16000)
+    write(sgmse_dir+filename, x_hat.cpu().numpy(), 16000)
     
     
