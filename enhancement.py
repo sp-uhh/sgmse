779bf40236dcbf377a8cba57de1473f4e140500c
Julius Richter <jrichter@informatik.uni-hamburg.de>
2022-06-24 17:51:32 +0200

    rename scripts

---
+++ enhancement.py
@@ -0,0 +1,85 @@
+import numpy as np
+import glob
+from soundfile import read, write
+from tqdm import tqdm
+from pesq import pesq
+from torchaudio import load
+import torch
+from argparse import ArgumentParser
+
+from sgmse.data_module import SpecsDataModule
+from sgmse.sdes import OUVESDE
+from sgmse.model import ScoreModel
+
+from utils import pad_spec, ensure_dir
+
+
+if __name__ == '__main__':
+    parser = ArgumentParser()
+    parser.add_argument("test", type=str, help="Specify test set.")
+    parser.add_argument("train", type=str, help="Specify train set.")
+    parser.add_argument("--test_dir", type=str, required=True, help='Directory containing the test data')
+    parser.add_argument("--ckpt", type=str,  help='Path to model checkpoint.')
+    parser.add_argument("--corrector", type=str, choices=("ald", "none"), default="ald", 
+        help="Corrector class for the PC sampler.")
+    parser.add_argument("--corrector_steps", type=int, default=1, help="Number of corrector steps")
+    parser.add_argument("--snr", type=float, default=0.33, help="SNR value for annealed Langevin dynmaics.")
+    parser.add_argument("--N", type=int, default=30, help="Number of reverse steps")
+    args = parser.parse_args()
+
+    noisy_dir = args.test_dir
+    checkpoint_file = args.ckpt
+    corrector_cls = args.corrector
+
+    target_dir = "/export/home/jrichter/repos/sgmse/enhanced/test_{}/train_{}/".format(
+        args.test, args.train) 
+
+    ensure_dir(target_dir)
+
+    # Settings
+    sr = 16000
+    snr = args.snr
+    N = args.N
+    corrector_steps = args.corrector_steps
+
+    # Load score model 
+    model = ScoreModel.load_from_checkpoint(
+        checkpoint_file, base_dir='/export/home/jrichter/data/wsj0_chime3/',
+        batch_size=16, num_workers=0, kwargs=dict(gpu=False)
+    )
+    model.eval(no_ema=False)
+    model.cuda()
+
+    noisy_files = sorted(glob.glob('{}/*.wav'.format(noisy_dir)))
+
+    for noisy_file in tqdm(noisy_files):
+        filename = noisy_file.split('/')[-1]
+        
+        # Load wav
+        y, _ = load(noisy_file) 
+        T_orig = y.size(1)   
+
+        # Normalize
+        norm_factor = y.abs().max()
+        y = y / norm_factor
+        
+        # Prepare DNN input
+        Y = torch.unsqueeze(model._forward_transform(model._stft(y.cuda())), 0)
+        Y = pad_spec(Y)
+        
+        # Reverse sampling
+        sampler = model.get_pc_sampler(
+            'reverse_diffusion', corrector_cls, Y.cuda(), N=N, 
+            corrector_steps=corrector_steps, snr=snr)
+        sample = sampler()
+        
+        # Backward transform in time domain
+        x_hat = model.to_audio(sample.squeeze(), T_orig)
+
+        # Renormalize
+        x_hat = x_hat * norm_factor
+
+        # Write enhanced wav file
+        write(target_dir+filename, x_hat.cpu().numpy(), 16000)
+        
+        
--- evaluation.py
+++ evaluation.py
@@ -1,89 +1,112 @@
+from os.path import join 
 import numpy as np
-import glob
+from glob import glob
 from soundfile import read, write
 from tqdm import tqdm
 from pesq import pesq
-from torchaudio import load
-import torch
+import pandas as pd
+from uhh_sp.evaluation import polqa
+from pystoi import stoi
+from pysepm import composite, SNRseg
 from argparse import ArgumentParser
+from __paths__ import wsj0_dir, vb_dir
 
-from sgmse.data_module import SpecsDataModule
-from sgmse.sdes import OUVESDE
-from sgmse.model import ScoreModel
-
-from utils import pad_spec, ensure_dir
+from utils import energy_ratios, mean_conf_int, Method, si_sdr, mean_std
 
 
 if __name__ == '__main__':
     parser = ArgumentParser()
-    parser.add_argument("test", type=str, help="Specify test set.")
-    parser.add_argument("train", type=str, help="Specify train set.")
-    parser.add_argument("--test_dir", type=str, required=True, help='Directory containing the test data')
-    parser.add_argument("--ckpt", type=str,  help='Path to model checkpoint.')
+    parser.add_argument("--test", nargs="+", required=True, default=[], 
+        help="Specify list of test set identifier: 'base_dir/test_\{test\}")
+    parser.add_argument("--train", nargs="+", required=True, default=[], 
+        help="Specify list of train set identifier: 'base_dir/train_\{train\}'.")
+    parser.add_argument("--base", nargs="+", required=True, default=[], 
+        help="Specify the base dir.")
+    parser.add_argument("--basic", action="store_true", default=False, help="Evaluate only the basic metrics")
     args = parser.parse_args()
 
-    noisy_dir = args.test_dir
+    base_dirs = args.base
+    basic = args.basic
 
-    target_dir = "/export/home/jrichter/repos/sgmse/enhanced/test_{}/train_{}/".format(
-        args.test, args.train) 
+    # options: "wsj0" or "vb"
+    test_set = args.test
+    train_set = args.train
 
-    ensure_dir(target_dir)
+    for i, base_dir in enumerate(base_dirs):
+        if test_set[i][:4] == "wsj0":
+            clean_dir = join(wsj0_dir, "test/clean")
+            noisy_dir = join(wsj0_dir, "test/noisy")
+        elif test_set[i][:2] == "vb":
+            clean_dir = join(vb_dir, "valid/clean")
+            noisy_dir = join(vb_dir, "valid/noisy")
+            
+        enhanced_dir = join(base_dir, f"test_{test_set[i]}", f"train_{train_set[i]}")
+        #enhanced_dir = "/export/home/jrichter/data/wsj0_chime3/test/noisy" # for the mixture
 
-    # SGMSE
-    #checkpoint_file = '/export/home/jrichter/repos/score-speech/sgmse_logs/SGMSE/sweet-resonance-97/epoch=325-step=39445.ckpt'
-    # VB-DMD
-    #checkpoint_file = '/export/home/jrichter/repos/sgmse/logs/sgmse/2haceomy/checkpoints/epoch=401-step=291047.ckpt'
-    # WSJ0
-    #checkpoint_file = '/export/home/jrichter/repos/sgmse/logs/sgmse/3hpyr94h/checkpoints/epoch=999-step=399999.ckpt'
-    # dns_chime
-    #checkpoint_file = '/export/home/jrichter/repos/sgmse/logs/sgmse/1dbja6ni/checkpoints/epoch=222-step=174831.ckpt'
+        if basic:
+            data = {"filename": [], "pesq": [], "estoi": [], "si_sdr": []}
+        else:
+            data = {"filename": [], "pesq": [], "estoi": [], "si_sdr": [], "si_sir": [], 
+                "si_sar": [], "csig": [], "cbak": [], "covl": [], "ssnr": [], }
+        sr = 16000
 
-    checkpoint_file = args.ckpt
+        # Evaluate standard metrics
+        noisy_files = sorted(glob('{}/*.wav'.format(noisy_dir)))
+        for noisy_file in tqdm(noisy_files):
+            filename = noisy_file.split('/')[-1]
+            x, _ = read(join(clean_dir, filename))
+            y, _ = read(noisy_file)
+            n = y - x 
+            x_method, _ = read(join(enhanced_dir, filename))
+            
+            data["filename"].append(filename)
+            data["pesq"].append(pesq(sr, x, x_method, 'wb'))
+            data["estoi"].append(stoi(x, x_method, sr, extended=True))
+            data["si_sdr"].append(energy_ratios(x_method, x, n)[0])
+            if not basic:
+                data["si_sir"].append(energy_ratios(x_method, x, n)[1])
+                data["si_sar"].append(energy_ratios(x_method, x, n)[2])
+                data["csig"].append(composite(x, x_method, sr)[0])
+                data["cbak"].append(composite(x, x_method, sr)[1])
+                data["covl"].append(composite(x, x_method, sr)[2])
+                data["ssnr"].append(SNRseg(x, x_method, sr))
 
-    # Settings
-    sr = 16000
-    snr = 0.33
-    N = 50
-    corrector_steps = 1
+        # Save results as DataFrame    
+        df = pd.DataFrame(data)
 
-    # Load score model 
-    model = ScoreModel.load_from_checkpoint(
-        checkpoint_file, base_dir='/export/home/jrichter/data/wsj0_chime3/',
-        batch_size=16, num_workers=0, kwargs=dict(gpu=False)
-    )
-    model.eval(no_ema=False)
-    model.cuda()
+        # POLQA evaluation
+        if not basic:
+            clean_files = sorted(glob('{}/*.wav'.format(clean_dir)))
+            enhanced_files = sorted(glob('{}/*.wav'.format(enhanced_dir)))
 
-    noisy_files = sorted(glob.glob('{}/*.wav'.format(noisy_dir)))
+            clean_audios = [read(clean_file)[0] for clean_file in clean_files]
+            enhanced_audios = [read(enhanced_file)[0] for enhanced_file in enhanced_files]
 
-    for noisy_file in tqdm(noisy_files):
-        filename = noisy_file.split('/')[-1]
-        
-        # Load wav
-        y, _ = load(noisy_file) 
-        T_orig = y.size(1)   
+            polqa_vals = polqa(clean_audios, enhanced_audios, 16000, save_to=None)
+            polqa_vals = [val[1] for val in polqa_vals]
 
-        # Normalize
-        norm_factor = y.abs().max()
-        y = y / norm_factor
-        
-        # Prepare DNN input
-        Y = torch.unsqueeze(model._forward_transform(model._stft(y.cuda())), 0)
-        Y = pad_spec(Y)
-        
-        # Reverse sampling
-        sampler = model.get_pc_sampler(
-            'reverse_diffusion', 'ald', Y.cuda(), N=N, 
-            corrector_steps=corrector_steps, snr=snr)
-        sample = sampler()
-        
-        # Backward transform in time domain
-        x_hat = model.to_audio(sample.squeeze(), T_orig)
+            # Add POLQA column to DataFrame
+            df['polqa'] = polqa_vals
 
-        # Renormalize
-        x_hat = x_hat * norm_factor
+        # Print results
+        if basic:
+            print(enhanced_dir)
+            print("PESQ: {:.2f} ± {:.2f}".format(*mean_std(df["pesq"].to_numpy())))
+            print("ESTOI: {:.2f} ± {:.2f}".format(*mean_std(df["estoi"].to_numpy())))
+            print("SI-SDR: {:.1f} ± {:.1f}".format(*mean_std(df["si_sdr"].to_numpy())))
+        else:
+            print(enhanced_dir)
+            print("POLQA: {:.2f} ± {:.2f}".format(*mean_std(df["polqa"].to_numpy())))
+            print("PESQ: {:.2f} ± {:.2f}".format(*mean_std(df["pesq"].to_numpy())))
+            print("ESTOI: {:.2f} ± {:.2f}".format(*mean_std(df["estoi"].to_numpy())))
+            print("SI-SDR: {:.1f} ± {:.1f}".format(*mean_std(df["si_sdr"].to_numpy())))
+            print("SI-SIR: {:.1f} ± {:.1f}".format(*mean_std(df["si_sir"].to_numpy())))
+            print("SI-SAR: {:.1f} ± {:.1f}".format(*mean_std(df["si_sar"].to_numpy())))
 
-        # Write enhanced wav file
-        write(target_dir+filename, x_hat.cpu().numpy(), 16000)
-        
+        # Save DataFrame as csv file
+        if basic:
+            df.to_csv(join(enhanced_dir, "_results_basic.csv"), index=False)
+        else:
+            df.to_csv(join(enhanced_dir, "_results.csv"), index=False)
+
         
