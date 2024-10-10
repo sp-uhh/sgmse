07a073c4d387a0cc2dda89e229b6ff2dc24d791f
jrichter <jrichter@exchange.informatik.uni-hamburg.de>
2024-10-10 15:55:35 +0200

    sampler_type arg, read flac, SBVE

--- enhancement.py
+++ enhancement.py
@@ -21,25 +21,33 @@ if __name__ == '__main__':
     parser.add_argument("--test_dir", type=str, required=True, help='Directory containing the test data')
     parser.add_argument("--enhanced_dir", type=str, required=True, help='Directory containing the enhanced data')
     parser.add_argument("--ckpt", type=str,  help='Path to model checkpoint')
+    parser.add_argument("--sampler_type", type=str, default="pc", help="Sampler type for the PC sampler.")
     parser.add_argument("--corrector", type=str, choices=("ald", "langevin", "none"), default="ald", help="Corrector class for the PC sampler.")
     parser.add_argument("--corrector_steps", type=int, default=1, help="Number of corrector steps")
     parser.add_argument("--snr", type=float, default=0.5, help="SNR value for (annealed) Langevin dynmaics")
     parser.add_argument("--N", type=int, default=30, help="Number of reverse steps")
     parser.add_argument("--device", type=str, default="cuda", help="Device to use for inference")
+    parser.add_argument("--t_eps", type=float, default=0.03, help="The minimum process time (0.03 by default)")
     args = parser.parse_args()
 
     # Load score model 
     model = ScoreModel.load_from_checkpoint(args.ckpt, map_location=args.device)
+    model.t_eps = args.t_eps
     model.eval()
 
     # Get list of noisy files
     noisy_files = []
     noisy_files += sorted(glob.glob(join(args.test_dir, '*.wav')))
     noisy_files += sorted(glob.glob(join(args.test_dir, '**', '*.wav')))
+    noisy_files += sorted(glob.glob(join(args.test_dir, '*.flac')))
+    noisy_files += sorted(glob.glob(join(args.test_dir, '**', '*.flac')))
 
     # Check if the model is trained on 48 kHz data
     if model.backbone == 'ncsnpp_48k':
         target_sr = 48000
+        pad_mode = "reflection"
+    elif model.backbone == 'ncsnpp_v2':
+        target_sr = 16000
         pad_mode = "reflection"
     else:
         target_sr = 16000
@@ -68,9 +76,19 @@ if __name__ == '__main__':
         Y = pad_spec(Y, mode=pad_mode)
         
         # Reverse sampling
-        sampler = model.get_pc_sampler(
-            'reverse_diffusion', args.corrector, Y.to(args.device), N=args.N, 
-            corrector_steps=args.corrector_steps, snr=args.snr)
+        if model.sde.__class__.__name__ == 'OUVESDE':
+            if args.sampler_type == 'pc':
+                sampler = model.get_pc_sampler('reverse_diffusion', args.corrector, Y.to(args.device), N=args.N, 
+                    corrector_steps=args.corrector_steps, snr=args.snr)
+            elif args.sampler_type == 'ode':
+                sampler = model.get_ode_sampler(Y.to(args.device), N=args.N)
+            else:
+                raise ValueError(f"Sampler type {args.sampler_type} not supported")
+        elif model.sde.__class__.__name__ == 'SBVESDE':
+            sampler = model.get_sb_sampler(sde=model.sde, y=Y.cuda(), sampler_type=args.sampler_type)
+        else:
+            raise ValueError(f"SDE {model.sde.__class__.__name__} not supported")
+            
         sample, _ = sampler()
         
         # Backward transform in time domain
